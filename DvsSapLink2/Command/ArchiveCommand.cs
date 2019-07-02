c2a17396064b77047c99fc1200251b3d0ec5354a
Markus Gilli <markus.gilli@schindler.com>
2019-07-02 12:49:51 +0200

    Refactoring, Cleanup: SapTransferWriter

--- DvsSapLink2/Command/ArchiveCommand.cs
+++ DvsSapLink2/Command/ArchiveCommand.cs
@@ -41,14 +41,13 @@ namespace DvsSapLink2.Command
 
             using (var logger = new Logger(logFile))
             {
-                //foreach (var attribute in this.attributesToLog)
-                //{
-                //    logger.Write("INFO", $"{attribute}: {file[attribute]}");
-                //}
-
-                using (var sapTransferWriter = new SapTransferWriter(sapTransferFileTemp))
+                using (var stream = new StreamWriter(sapTransferFileTemp))
                 {
-                    sapTransferWriter.WriteFileAttributes(file, viewModel.Sap.Data);
+                    this.WriteFileAttributes(stream, file, viewModel.Sap.Data);
+                    this.WriteDocumentInfo(stream, file, "DOKTYP_ZAB_D", "mechanische Zeichnung");
+                    this.WriteDocumentInfo(stream, file, "PROJ_D", file[FileAttributeName.Typ]);
+                    this.WriteDocumentInfo(stream, file, "AUFTR_NR_D", file[FileAttributeName.AuftragsNummer]);
+                    this.WriteDocumentInfo(stream, file, "FORMAT_D", file[FileAttributeName.Format]);
                 }
 
                 //TODO: Text durch Textkonstante ersetzen
@@ -68,6 +67,52 @@ namespace DvsSapLink2.Command
             
             // HACK: force update of file list
             viewModel.Configuration.SourceDirectory = viewModel.Configuration.SourceDirectory;
+        }
+
+        /// <summary>
+        /// Writes a log entry
+        /// </summary>
+        /// <param name="stream">Stream of the file to write</param>
+        /// <param name="file">Attribute file to write into the file</param>
+        /// <param name="sapData">SAP data to write into the file</param>
+        private void WriteFileAttributes(StreamWriter stream, AttributeFile file, SapData sapData)
+        {
+            var A = "DOK01";                                                    //   1 Recordart
+            var B = file[FileAttributeName.ZeichnungsNummer];                   //   7 Dokument-Nummer          HTAM123456
+            var C = "ZAB";                                                      //  32 Dokument-Art             ZAB
+            var D = file[FileAttributeName.AeStand_aktuell];                    //  35 Dokument-Version         A
+            var E = $"D{int.Parse(file[FileAttributeName.BlattNr]):D2}";        //  37 Teildokument             D01
+            var F = "D";                                                        //  40 Sprache                  D
+            var G = " ";                                                        //  41 Änderungs-Nr.
+            var H = file[FileAttributeName.Haupttitel];                         //  53 Titel                    Massbild
+            var I = "CAD-DVS-Update";                                           // 308 Änderungsbeschreib.      CAD-DVS-Update
+            var J = sapData.State;                                              // 378 Dokument-Status          DR
+            var K = sapData.User;                                               // 380 Sachbearbeiter           221226
+            var L = sapData.Labor.ToString();                                   // 392 Labor/Büro               760
+            var M = "ACD";                                                      // 395 Datei 1                  ACD
+            var N = "";                                                         // 398 Datenträger 1
+            var O = "";                                                         // 408 File-Name 1
+            var P = "PDF";                                                      // 478 Datei 2                  PDF         (TIF)
+            var Q = "IM_PRE_V";                                                 // 481 Datenträger 2            IM_PRE_V    (IM_CAD_V)
+            var R = file.Title + ".tif";                                        // 491 File-Name 2              HTAM123456-0-01.tif
+            var S = "LABOR/BÜRO";                                               // 586 Bezugsort SAP            LABOR/BÜRO
+
+            var line = $"{A,-6}{B,-25}{C,-3}{D,-2}{E,-3}{F,-1}{G,-12}{H,-255}{I,-70}{J,-2}{K,-12}{L,-3}{M,-3}{N,-10}{O,-70}{P,-3}{Q,-10}{R,-95}{S,-14}";
+            stream.WriteLine(line);
+        }
+
+        /// <summary>
+        /// Write document information as fix width attributes into the file
+        /// </summary>
+        private void WriteDocumentInfo(StreamWriter stream, AttributeFile file, string type, string changeNr)
+        {
+            var record = "DOK02";
+            var number = file[FileAttributeName.ZeichnungsNummer];
+            var docType = "ZAB";
+            var version = file[FileAttributeName.AeStand_aktuell];
+            var page = $"D{int.Parse(file[FileAttributeName.BlattNr]):D2}";
+
+            stream.WriteLine($"{record,-6}{number,-25}{docType,-3}{version,-2}{page,-3}{type,-30}{changeNr,-530}");
         }
     }
 }
--- DvsSapLink2/Helper/Logger.cs
+++ DvsSapLink2/Helper/Logger.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
-using System.Linq;
-using DvsSapLink2.Model;
+
 namespace DvsSapLink2.Helper
 {
     public class Logger : IDisposable
@@ -35,100 +34,6 @@ namespace DvsSapLink2.Helper
         public void Dispose()
         {
             this.stream?.Dispose();
-        }
-    }
-
-    public class SapTransferWriter : IDisposable
-    {
-        private readonly StreamWriter streamSapTransfer;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Logger"/> class.
-        /// </summary>
-        public SapTransferWriter(string path)
-        {
-            this.streamSapTransfer = new StreamWriter(path);
-        }
-
-        /// <summary>
-        /// Writes a log entry
-        /// </summary>
-        /// <param name="key">Log entry key</param>
-        /// <param name="message">Message to log</param>
-        public void WriteFileAttributes(AttributeFile attributeFile, SapData sapData)
-        {
-
-            // Zeile 1                                                                      Start-Pos, Beschreibung, Beispiel
-            string A = "DOK01";                                                             //   1 Recordart
-            string B = attributeFile[FileAttributeName.ZeichnungsNummer];                   //   7 Dokument-Nummer          HTAM123456
-            string C = "ZAB";                                                               //  32 Dokument-Art             ZAB
-            string D = attributeFile[FileAttributeName.AeStand_aktuell];                    //  35 Dokument-Version         A
-            string E = string.Format("D{0:D2}", 
-                       int.Parse(attributeFile[FileAttributeName.BlattNr]));                //  37 Teildokument             D01
-            string F = "D";                                                                 //  40 Sprache                  D
-            string G = " ";                                                                 //  41 Änderungs-Nr.        
-            string H = attributeFile[FileAttributeName.Haupttitel];                         //  53 Titel                    Massbild
-            string I = "CAD-DVS-Update";                                                    // 308 Änderungsbeschreib.      CAD-DVS-Update
-            string J = sapData.State;                                                       // 378 Dokument-Status          DR
-            string K = sapData.User;                                                        // 380 Sachbearbeiter           221226
-            string L = sapData.Labor.ToString();                                            // 392 Labor/Büro               760
-            string M = "ACD";                                                               // 395 Datei 1                  ACD
-            string N = "";                                                                  // 398 Datenträger 1           
-            string O = "";                                                                  // 408 File-Name 1
-            string P = "PDF";                                                               // 478 Datei 2                  PDF         (TIF)
-            string Q = "IM_PRE_V";                                                          // 481 Datenträger 2            IM_PRE_V    (IM_CAD_V)
-            string R = attributeFile.Title + ".tif";                                        // 491 File-Name 2              HTAM123456-0-01.tif
-            string S = "LABOR/BÜRO";                                                        // 586 Bezugsort SAP            LABOR/BÜRO
-
-            var line = $"{A:-6}{B:-25}{C:-3}{D:-2}{E:-3}{F:-1}{G:-12}{H:-255}{I:-70}{J:-2}{K:-12}{L:-3}{M:-3}{N:-10}{O:-70}{P:-3}{Q:-10}{R:-95}{S:-14}";
-            this.streamSapTransfer.WriteLine(line);
-
-
-            // Zeile 2
-            A = "DOK02";                                                                    //   1 Recordart
-            F = "DOKTYP_ZAB_D";                                                             //  40 Merkmal Dokumenten-Typ   DOKTYP_ZAB_D
-            G = "mechanische Zeichnung";                                                    //  70 Merkmalwert              mechanische Zeichnung
-
-            line = $"{A,-6}{B,-25}{C,-3}{D,-2}{E,-3}{F,-30}{G,-530}";
-            this.streamSapTransfer.WriteLine(line);
-
-
-            // Zeile 3
-            A = "DOK02";                                                                    //   1 Recordart
-            F = "PROJ_D";                                                                   //  40 Merkmal Dokumenten-Typ   PROJ_D
-            G = attributeFile[FileAttributeName.Typ];                                       //  70 Merkmalwert              uQWG+ 630...
-
-            line = $"{A,-6}{B,-25}{C,-3}{D,-2}{E,-3}{F,-30}{G,-530}";
-            this.streamSapTransfer.WriteLine(line);
-
-
-            // Zeile 4
-            A = "DOK02";                                                                    //   1 Recordart
-            F = "AUFTR_NR_D";                                                               //  40 Merkmal Dokumenten-Typ   AUFTR_NR_D
-            G = attributeFile[FileAttributeName.AuftragsNummer];                            //  70 Merkmalwert              11005084-000101
-
-            line = $"{A,-6}{B,-25}{C,-3}{D,-2}{E,-3}{F,-30}{G,-530}";
-            this.streamSapTransfer.WriteLine(line);
-
-
-            // Zeile 5
-            A = "DOK02";                                                                    //   1 Recordart
-            F = "FORMAT_D";                                                                 //  40 Merkmal Dokumenten-Typ   FORMAT_D
-            G = attributeFile[FileAttributeName.Format];                                    //  70 Merkmalwert              11005084-000101
-
-            line = $"{A,-6}{B,-25}{C,-3}{D,-2}{E,-3}{F,-30}{G,-530}";
-            this.streamSapTransfer.WriteLine(line);
-
-
-            this.streamSapTransfer.Flush();
-        }
-
-        /// <summary>
-        /// Releases all resources used by this <see cref="Logger"/> instance
-        /// </summary>
-        public void Dispose()
-        {
-            this.streamSapTransfer?.Dispose();
         }
     }
 }
